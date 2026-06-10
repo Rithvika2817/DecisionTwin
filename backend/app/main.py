81559d81f99520ec12fe91d494877b3fd47000d4
Ankireddypalli Venkata Rithvika <arithvika2006@gmail.com>
2026-06-10 18:03:37 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.routers import (
     health,
@@ -10,6 +11,15 @@ from app.routers import (
 )
 
 app = FastAPI(title="DecisionTwin API")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Routes
 app.include_router(health.router)
